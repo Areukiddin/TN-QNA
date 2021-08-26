fa906e8ac7311dc9f3a7b9fc5633cb8b19bdd425
Evgeny Maximov <eu.max@voiso.com>
2021-08-26 12:07:07 +0300

    part 2

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :questions
 
-  has_one_attached :file
   has_many :answers, -> { order(best: :desc) }, inverse_of: :question, dependent: :destroy
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
