c866086cd700a215e07e20add90fb60e446de8b6
Evgeny Maximov <eu.max@voiso.com>
2021-09-03 17:25:22 +0300

    add links to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,11 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', inverse_of: :questions
 
   has_many :answers, -> { order(best: :desc) }, inverse_of: :question, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :title, :body, presence: true
 end
